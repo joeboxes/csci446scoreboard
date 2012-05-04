b065bb6122c1584f85c8dcc311df5ce079452987
Richie <zirbsster@gmail.com>
2012-05-03 22:03:54 -0600

    GOT SCOREBORAD WORKING ON 12.04

---
+++ config/initializers/cookie_verification_secret.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+ActionController::Base.cookie_verifier_secret = '9a4c61eb8841ccb5bf01813f8ab1c91e987f21df884d4a080ed7f4be7c6cb0f1a5d80b59286513b387d6f3f31f95fe753a63610fc3b5cfc22a8c1e9be9efffed';
